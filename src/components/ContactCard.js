8d02308b7520ddb26764a04cce79644925dd7343
Yungchih Chen <yungchih_chen@yahoo.com>
2019-02-24 11:48:18 -0500

    Add style to contactCard

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import { Card, Button, Icon } from 'semantic-ui-react'
+import { Card, Button, Icon, Image} from 'semantic-ui-react'
 
 export default function ContactCard({contact}) {
-  const { id, first_name, last_name, phone, email } = contact
+  const { id, first_name, last_name, phone, email, avator } = contact
   return (
     <Card>
       <Card.Content>
+        <Image floated='right' size='mini' src={avator} />
         <Card.Header>
-          <Icon name='user outline'>
-            { first_name } { last_name }
-          </Icon>
+          { first_name } { last_name }
         </Card.Header>
         <Card.Description>
           <p><Icon name='phone'>{ phone }</Icon></p>
-          <p><Icon name='phone'>{ email }</Icon></p>
+          <p><Icon name='mail outline'>{ email }</Icon></p>
         </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>Edit</Button>
+          <Button basic color='red'>Delete</Button>
+        </div>
       </Card.Content>
     </Card>
   )
