da7452040cc3e1c51b7d43b9ad228e9d5fb8922e
Yungchih Chen <yungchih_chen@yahoo.com>
2019-03-01 23:19:03 -0500

    Add semantic-ui-react Form, Grid and Button

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Form, Grid, Button } from 'semantic-ui-react';
 
 const submit = (values) => {
   console.log('inside of the contactForm')
   console.log(values)
 }
-// decompose field to { input, meta: {touched, error}}
+// decompose field to { input, label, type, meta: {touched, error}}
 const renderField = ({input, label, type, meta: { touched, error}}) => (
-  <div className="input-row">
+  <Form.Field className={{error:touched && error}}>
     <label>{label}</label>
     <input {...input} placeholder={label} type={type} />
-    { touched && error && <span className="error">{error}</span> }
-  </div>
+    { touched && error && <span className="error">{error.message}</span> }
+  </Form.Field>
 )
 
 const ContactFormFunc = ({handleSubmit}) => (
-    <form onSubmit={handleSubmit(submit)}>
-      <div>
+  <Grid centered columns={2}>
+    <Grid.Column>
+    <Form onSubmit={handleSubmit(submit)}>
+      <Form.Group widths='equal'>
         <Field name="firstName" label="First Name" component={renderField} type="text"/>
-      </div>
-      <div>
         <Field name="lastName" label="Last Name" component={renderField} type="text"/>
-      </div>
-      <div>
-        <Field name="phone" label="Phone" component={renderField} type="text"/>
-      </div>
-      <div>
-        <Field name="email" label="Email" component={renderField} type="text"/>
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      </Form.Group>
+      <Field name="phone" label="Phone" component={renderField} type="text"/>
+      <Field name="email" label="Email" component={renderField} type="text"/>
+      <Button primary type="submit">Save</Button>
+    </Form>
+    </Grid.Column>
+  </Grid>
   )
 
 const ContactForm = reduxForm({
