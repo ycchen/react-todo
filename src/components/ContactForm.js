bfb6e905e051b122ed61b3b61078c7ad4e8c2844
Yungchih Chen <yungchih_chen@yahoo.com>
2019-03-01 23:09:04 -0500

    Add refactor form to use custom renderField function

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,47 +1,41 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
-class ContactForm extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       contact: {}
-    }
-  }
-  submit = (values) => {
-    console.log('inside of the contactForm')
-    console.log(values)
-  }
-  
-  render() {
-    return (
-      <form onSubmit={this.props.handleSubmit(this.submit)}>
-       <label htmlFor="firstName">First Name</label>
-        <div>
-          <Field name="firstName" component="input" type="text"/>
-        </div>
-        <label htmlFor="lastName">Last Name</label>
-        <div>
-          <Field name="lastName" component="input" type="text"/>
-        </div>
-        <label htmlFor="phone">Phone</label>
-        <div>
-          <Field name="phone" component="input" type="text"/>
-        </div>
-        <label htmlFor="email">Email</label>
-        <div>
-          <Field name="email" component="input" type="text"/>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    )
-  }
-}
 
-ContactForm = reduxForm({
+const submit = (values) => {
+  console.log('inside of the contactForm')
+  console.log(values)
+}
+// decompose field to { input, meta: {touched, error}}
+const renderField = ({input, label, type, meta: { touched, error}}) => (
+  <div className="input-row">
+    <label>{label}</label>
+    <input {...input} placeholder={label} type={type} />
+    { touched && error && <span className="error">{error}</span> }
+  </div>
+)
+
+const ContactFormFunc = ({handleSubmit}) => (
+    <form onSubmit={handleSubmit(submit)}>
+      <div>
+        <Field name="firstName" label="First Name" component={renderField} type="text"/>
+      </div>
+      <div>
+        <Field name="lastName" label="Last Name" component={renderField} type="text"/>
+      </div>
+      <div>
+        <Field name="phone" label="Phone" component={renderField} type="text"/>
+      </div>
+      <div>
+        <Field name="email" label="Email" component={renderField} type="text"/>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
+
+const ContactForm = reduxForm({
   form: 'contact'
-})(ContactForm)
+})(ContactFormFunc)
 
 export default ContactForm
